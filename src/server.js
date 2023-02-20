a35d7956956c675403b365c12bd04085884066ed
Michael Z <michaelzwierzanski@gmail.com>
2023-02-20 15:55:36 +0000

    add get and post route

--- src/server.js
+++ src/server.js
@@ -1,20 +1,50 @@
 //import express from "../node_modules/....."
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
+const app = express();
 
-// app.use("/", express.static("blank"));
-// app.use("/books", express.static("books"));
+app.use(express.json());
 
-// app.listen(5001, () => console.log("Server is listening on port 5001"));
+app.get("/book", (request, response) => {
+  //   console.log(request);
+  //   response.send("Hello from the book route!");
+  const book = {
+    title: "lord of the rings",
+    author: "tolkein",
+    genre: "fantasy",
+  };
 
-// const express = require("express");
+  const successResponse = {
+    message: "response sent successfully",
+    book: book,
+  };
 
-// const app = express();
-// //tells express that any function starting app. is using the express library
+  response.send(successResponse);
+});
 
-// app.use("/books", express.static("books"));
-// //This tells express that if a user choose an endpoint of /books on the end of the server address then to run a web static server with the index.html file located in the "books" folder.
+// {
+//   "title": "matilda",
+//   "author": "roald dahl",
+//   "genre": "childrens"
+// }
 
-// app.listen(5001, () => console.log("Server listening on port 5001"));
-// // app.listen runs the listen method from express. The listen method takes in a TCP/IP port, in this case 5001 and then runs an initial function. In this case the initial function is just a message to say that express is listening on port 5001. When listening express can react to any requests that come in on port 5001.
+app.post("/book", (request, response) => {
+  //   console.log(request.body);
+  //   response.send("Hello from the post route!");
+
+  const newBook = {
+    id: Math.floor(Math.random() * 50),
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  };
+
+  const successResponse = {
+    message: "Successfully added to DB",
+    newBook: newBook,
+  };
+
+  response.send(successResponse);
+});
+
+app.listen(5001, () => console.log("Server is listening on port 5001"));
