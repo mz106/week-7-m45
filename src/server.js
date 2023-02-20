aadeedb5e0dabce49304333a4541b3922f19f536
Michael Z <michaelzwierzanski@gmail.com>
2023-02-20 14:35:39 +0000

    add notes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 //import express from "../node_modules/....."
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+// const app = express();
 
-app.use("/", express.static("blank"));
-app.use("/books", express.static("books"));
+// app.use("/", express.static("blank"));
+// app.use("/books", express.static("books"));
 
-app.listen(5001, () => console.log("Server is listening on port 5001"));
+// app.listen(5001, () => console.log("Server is listening on port 5001"));
+
+// const express = require("express");
+
+// const app = express();
+// //tells express that any function starting app. is using the express library
+
+// app.use("/books", express.static("books"));
+// //This tells express that if a user choose an endpoint of /books on the end of the server address then to run a web static server with the index.html file located in the "books" folder.
+
+// app.listen(5001, () => console.log("Server listening on port 5001"));
+// // app.listen runs the listen method from express. The listen method takes in a TCP/IP port, in this case 5001 and then runs an initial function. In this case the initial function is just a message to say that express is listening on port 5001. When listening express can react to any requests that come in on port 5001.
